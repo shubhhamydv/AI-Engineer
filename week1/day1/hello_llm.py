0d395dc1ee48d2adf667206f20421e2c89ad0677
shubhhamydv <shubhhamydv@gmail.com>
2026-09-03 20:22:39 +0530

    feat(day1): first Groq API call and basic chat completion

---
+++ week1/day1/hello_llm.py
@@ -0,0 +1,31 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+
+model="openai/gpt-oss-20b"
+role="user"
+prompt="Do you know codes.on.notes"
+# message me role and content
+message={
+    "role": role,
+    "content": prompt
+}
+
+messages=[message]
+
+response=client.chat.completions.create(model=model, messages=messages)
+print(response)
+
+print("#######################################")
+
+answer=response.choices[0].message.content
+print(answer)
